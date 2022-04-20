bdc065fd8cdf04ef81d794a359648778248b7298
Luiz Junior <lacjunior129.la@gmail.com>
2022-04-20 18:49:32 -0300

    requisito 8 concluído

--- script.js
+++ script.js
@@ -56,3 +56,41 @@ function classSelectedAddRemove() {
   green.addEventListener('click', addAndRemoveClassSelected);
 }
 classSelectedAddRemove();
+
+function colocaCorNosPixels() {
+    let black = document.querySelector('.black');
+    let red = document.querySelector('.red');
+    let blue = document.querySelector('.blue');
+    let green = document.querySelector('.green');
+    let selected = document.querySelector('.selected');
+    let pixel = document.getElementsByClassName('pixel');
+    
+    for(let index = 0; index < pixel.length; index += 1) {
+        pixel[index].addEventListener('click', function (event) {
+          if(black.classList.contains('selected')) {
+            pixel[index].classList.remove('red');
+            pixel[index].classList.remove('blue');
+            pixel[index].classList.remove('green');
+            event.target.classList.add('black');
+          } if(red.classList.contains('selected')) {
+            pixel[index].classList.remove('black');
+            pixel[index].classList.remove('blue');
+            pixel[index].classList.remove('green');
+            event.target.classList.add('red');
+          } if(blue.classList.contains('selected')) {
+            pixel[index].classList.remove('black');
+            pixel[index].classList.remove('red');
+            pixel[index].classList.remove('green');
+            event.target.classList.add('blue');
+          } if(green.classList.contains('selected')) {
+            pixel[index].classList.remove('black');
+            pixel[index].classList.remove('blue');
+            pixel[index].classList.remove('red');
+            event.target.classList.add('green');
+          } 
+        })
+    }
+}
+colocaCorNosPixels()
+
+
