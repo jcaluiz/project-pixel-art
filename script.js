0c35e3c3e195f28f39aacdc7750ce833333d769b
Luiz Junior <lacjunior129.la@gmail.com>
2022-04-20 16:53:53 -0300

    requisito 4 concluído

---
+++ script.js
@@ -0,0 +1,39 @@
+function createPixelInBord() {
+    function createPixel() {
+        let pixelBoard = document.getElementById('pixel-board');
+        let square = document.createElement('section');
+        square.className = 'pixel';
+        pixelBoard.appendChild(square)
+        let pixel = pixelBoard.appendChild(square);
+        pixel.style.display = 'inline-block'
+    }
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    createPixel();
+    
+}
+createPixelInBord();
+
+
