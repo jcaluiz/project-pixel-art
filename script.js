25acb5c1561db3e904311c266ef10aece2a8c6af
Luiz Junior <lacjunior129.la@gmail.com>
2022-04-23 12:17:39 -0300

    requisito 12 concluído

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function createPixelInBord() {
     let square = document.createElement('section');
     square.className = 'pixel';
     pixelBoard.appendChild(square)
-    let pixel = pixelBoard.appendChild(square);
-    pixel.style.display = 'inline-block'
   }
   createPixel();
   createPixel();
@@ -58,55 +56,129 @@ function classSelectedAddRemove() {
 classSelectedAddRemove();
 
 function colocaCorNosPixels() {
-    let black = document.querySelector('.black');
-    let red = document.querySelector('.red');
-    let blue = document.querySelector('.blue');
-    let green = document.querySelector('.green');
-    let selected = document.querySelector('.selected');
-    let pixel = document.getElementsByClassName('pixel');
-    
-    for(let index = 0; index < pixel.length; index += 1) {
-        pixel[index].addEventListener('click', function (event) {
-          if(black.classList.contains('selected')) {
-            pixel[index].classList.remove('red');
-            pixel[index].classList.remove('blue');
-            pixel[index].classList.remove('green');
-            pixel[index].classList.remove('white');
-            event.target.classList.add('black');
-          } if(red.classList.contains('selected')) {
-            pixel[index].classList.remove('black');
-            pixel[index].classList.remove('blue');
-            pixel[index].classList.remove('green');
-            pixel[index].classList.remove('white');
-            event.target.classList.add('red');
-          } if(blue.classList.contains('selected')) {
-            pixel[index].classList.remove('black');
-            pixel[index].classList.remove('red');
-            pixel[index].classList.remove('green');
-            pixel[index].classList.remove('white');
-            event.target.classList.add('blue');
-          } if(green.classList.contains('selected')) {
-            pixel[index].classList.remove('black');
-            pixel[index].classList.remove('blue');
-            pixel[index].classList.remove('red');
-            pixel[index].classList.remove('white');
-            event.target.classList.add('green');
-          } 
-        })
+  let black = document.querySelector('.black');
+  let red = document.querySelector('.red');
+  let blue = document.querySelector('.blue');
+  let green = document.querySelector('.green');
+  let selected = document.querySelector('.selected');
+  let pixel = document.getElementsByClassName('pixel');
+
+  function colorRandom() {
+    let numbersAndLetters = '0123456789ABCDEF';
+    let hashtag = '#';
+
+    for (let index = 0; index < 6; index += 1) {
+      hashtag += numbersAndLetters[Math.floor(Math.random() * 16)];
     }
+
+    return hashtag;
+    //ref: https://www.horadecodar.com.br/2022/01/16/gerar-cor-aleatoria-com-javascript/
+  }
+  let color1 = colorRandom();
+  let color2 = colorRandom();
+  let color3 = colorRandom();
+  let black1 = 'black';
+
+  let colorUm = black.style.backgroundColor = black1;
+
+  let colorDois = red.style.backgroundColor = color1;
+
+  let colorTres = blue.style.backgroundColor = color2;
+
+  let colorQuatro = green.style.backgroundColor = color3;
+  
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].addEventListener('click', function (event) {
+      if (black.classList.contains('selected')) {
+        pixel[index].classList.remove('red');
+        pixel[index].classList.remove('blue');
+        pixel[index].classList.remove('green');
+        pixel[index].classList.remove('white');
+        event.target.classList.add('black');
+        event.target.style.backgroundColor = colorUm;
+      }
+      if (red.classList.contains('selected')) {
+        pixel[index].classList.remove('black');
+        pixel[index].classList.remove('blue');
+        pixel[index].classList.remove('green');
+        pixel[index].classList.remove('white');
+        event.target.classList.add('red');
+        event.target.style.backgroundColor = colorDois;
+      }
+      if (blue.classList.contains('selected')) {
+        pixel[index].classList.remove('black');
+        pixel[index].classList.remove('red');
+        pixel[index].classList.remove('green');
+        pixel[index].classList.remove('white');
+        event.target.classList.add('blue');
+        event.target.style.backgroundColor = colorTres;
+      }
+      if (green.classList.contains('selected')) {
+        pixel[index].classList.remove('black');
+        pixel[index].classList.remove('blue');
+        pixel[index].classList.remove('red');
+        pixel[index].classList.remove('white');
+        event.target.classList.add('green');
+        event.target.style.backgroundColor = colorQuatro;
+      }
+    })
+  }
 }
 colocaCorNosPixels()
 
 function clearBoard() {
-    let clearBoard = document.querySelector('#clear-board');
-    let pixel = document.getElementsByClassName('pixel');
-    
-    clearBoard.addEventListener('click', function() {
-    for(let index = 0; index < pixel.length; index += 1) {
-            pixel[index].classList.add('white')
-        }
-    })
+  let clearBoard = document.querySelector('#clear-board');
+  let pixel = document.getElementsByClassName('pixel');
+  // let backgroundWhite = document.getElementsByClassName('white');
+  // backgroundWhite.style.backgroundColor = 'white';
+
+  clearBoard.addEventListener('click', function () {
+    for (let index = 0; index < pixel.length; index += 1) {
+      pixel[index].style.backgroundColor = 'white';
+    }
+  })
 }
 clearBoard();
 
+function pixelAlturaLargura() {
+  function createPixel() {
+    let pixelBoard = document.getElementById('pixel-board');
+    let square = document.createElement('section');
+    let alturaLargura = document.querySelectorAll('.pixel');
+    square.className = 'pixel';
+    pixelBoard.appendChild(square);
+    for (let index = 0; index < alturaLargura.length; index += 1) {
+      alturaLargura[index].style.height = parseInt(input.value) + 'px';
+      alturaLargura[index].style.width = parseInt(input.value) + 'px';
+    }
+  }
+  let input = document.querySelector('#board-size');
+  let button = document.querySelector('#generate-board');
+  let pixelBoard = document.querySelector('#pixel-board');
+  button.addEventListener('click', function () {
+    let alturaLargura = document.querySelectorAll('.pixel');
+    for (let index = 0; index < alturaLargura.length; index += 1) {
+      if (parseInt(input.value) > 4 && parseInt(input.value) < 51) {
+        alturaLargura[index].style.height = parseInt(input.value) + 'px';
+        alturaLargura[index].style.width = parseInt(input.value) + 'px';
+      }
+      if (parseInt(input.value) > 4 && parseInt(input.value) < 11) {
+        createPixel();
+        createPixel();
+        createPixel();
+      }
+      if (parseInt(input.value) > 10 && parseInt(input.value) < 21) {
+        createPixel();
+        createPixel();
+      }
+      if (parseInt(input.value) > 20 && parseInt(input.value) < 31) {
+        createPixel();
+      }
+      if (input.value === '') {
+        alert("Board inválido!");
+      }
 
+    }
+  })
+}
+pixelAlturaLargura();
